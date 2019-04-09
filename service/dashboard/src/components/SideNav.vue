2612f636b89e0410932f66d3d403f34f42914fe5
sorsaffari <soroush@grakn.ai>
2019-04-09 13:27:46 +0100

    restyle the navigation (side) bar

--- service/dashboard/src/components/SideNav.vue
+++ service/dashboard/src/components/SideNav.vue
@@ -10,17 +10,18 @@
       </div>
       <el-menu-item index="/overview">
         <i class="el-icon-menu" /> <span slot="title">
-          Overview
+          <span>Overview</span>
         </span>
       </el-menu-item>
       <el-menu-item index="/executions">
         <i class="el-icon-document" /> <span slot="title">
-          Executions
+          <span>Executions</span>
         </span>
       </el-menu-item>
     </el-menu>
   </aside>
 </template>
+
 <script>
 export default {
   data() {
@@ -29,38 +30,18 @@ export default {
   methods: {},
 };
 </script>
+
 <style scoped>
-.el-aside {
-  background-color: white;
-}
-img {
-  width: 130px;
-}
+.el-aside { background-color: white; }
+img { width: 130px; }
 .logo {
-  border-bottom: 1px solid rgba(102, 97, 91, 0.3);
+  border-bottom: 1px solid #ebeef5;
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 60px;
-  margin: 0 20px;
+  height: 47px;
+  padding: 0 20px;
 }
-.el-menu {
-  height: 100vh;
-}
-.el-menu-item.is-active {
-  color: #eb5e28;
-}
-.el-menu-item:focus {
-  background-color: transparent;
-}
-.is-active:before {
-  border-right: 17px solid rgb(233, 233, 233);
-  border-top: 17px solid transparent;
-  border-bottom: 17px solid transparent;
-  content: "";
-  display: inline-block;
-  position: absolute;
-  right: -4px;
-  top: 12px;
-}
+.el-menu { height: 100vh; }
+.el-menu-item:focus { background-color: transparent; }
 </style>

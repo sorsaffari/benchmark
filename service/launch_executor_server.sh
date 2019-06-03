e978fe582ac63922e35a6e303c067aa95a9b3b73
Marco Scoppetta <marco@grakn.ai>
2019-06-03 16:36:00 +0100

    use new gcloud image with updated bazel (#219)

--- service/launch_executor_server.sh
+++ service/launch_executor_server.sh
@@ -22,7 +22,7 @@ echo "Creating google cloud compute instance $INSTANCE_NAME..." | tee -a $LOG
 #TODO: update metadata for project ssh keys
 #for now it will keep trying to Updating project ssh metadata... and fail every time we try to ssh in it
 gcloud compute instances create $INSTANCE_NAME          \
-    --image-family grakn-benchmark-executor             \
+    --image benchmark-executor-image-2             \
     --image-project grakn-dev                           \
     --machine-type n1-standard-16                        \
     --zone=$ZONE                                        \

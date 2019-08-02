df822b090e7fba5d2e876f1cf574de199efd9be6
sorsaffari <soroush@grakn.ai>
2019-08-02 19:00:06 +0100

    misc

--- service/web-server/src/execution/middlewares.ts
+++ service/web-server/src/execution/middlewares.ts
@@ -1,4 +1,4 @@
-export const isPRMerged = (req, res, next) => {
+export const checkPRMerged = (req, res, next) => {
     const { body: { action, pull_request: { merged }}} = req;
     const isMerged = action === 'closed' && merged;
     isMerged ? next() : res.status(200).json({ triggered: false, error: 'PR has not been merged yet.' });

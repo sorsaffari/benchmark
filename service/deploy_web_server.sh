6d9d8994daf46d27c1187374ee6c4a7bfc840c91
Soroush Saffari <soroush@grakn.ai>
2019-09-04 17:09:41 +0100

    update deploy_web_server.sh

--- service/deploy_web_server.sh
+++ service/deploy_web_server.sh
@@ -17,14 +17,20 @@ echo "Getting to a clean web-server state on $INSTANCE_NAME..."
 gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='if ps -C node; then sudo pkill node; else echo "web-server is not running in the first place. proceeding."; fi'
 gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo rm -rf ~/service/web-server/'
 
-echo "Uploading web-server files to $INSTANCE_NAME..."
+echo "Preparing web-server content for upload"
+zip -r web-server.zip web-server -x web-server/node_modules/\*
 
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='mkdir -p ~/service/web-server/'
-gcloud compute scp --recurse web-server/src ubuntu@$INSTANCE_NAME:~/service/web-server/ --zone=$ZONE
-gcloud compute scp web-server/package.json ubuntu@$INSTANCE_NAME:~/service/web-server/ --zone=$ZONE
+echo "Uploading web-server to $INSTANCE_NAME..."
+
+gcloud compute scp web-server.zip ubuntu@$INSTANCE_NAME:~/service/ --zone=$ZONE
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='cd ~/service && unzip web-server.zip'
+
+echo "Post upload clean up"
+rm web-server.zip
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='rm ~/service/web-server.zip'
 
 echo "Installing dependencies"
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='cd ~/service/web-server && npm install'
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='cd ~/service/web-server && yarn install'
 
 echo "Starting the web-server"
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='nohup sudo node ~/service/web-server/src/server.js 2>&1 | tee -a ~/logs/node_server.log'
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='cd ~/service/web-server && nohup sudo NODE_ENV=production yarn start 2>&1 | tee -a ~/logs/node_server.log'
